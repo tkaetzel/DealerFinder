040af5c21a44f0fa8377acb919316dcd095ca2e7
tkaetzel <tkaetzel@dealeron.com>
2018-03-02 22:41:30 -0500

    initial commit

---
+++ DealerFinder/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DealerFinder.Controllers
+{
+    [Route("api/[controller]")]
+    public class OptionsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+          return Json(Model.Products.CategorizedProducts);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DealerFinder/Model/CategorizedProducts.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+
+namespace DealerFinder.Model
+{
+  public enum Category
+  {
+    AddOns,
+    PricingElements,
+    ChatProviders,
+    VdpTypes,
+    Tpis,
+    Widgets
+  }
+
+  [JsonConverter(typeof(StringEnumConverter))]
+  public enum Product
+  {
+    Bts,
+    PersonalizedOffer,
+    ChromeIncentives,
+    MobileLeadDriver,
+    MobileLeadDriverWithGeofencing,
+    ClassicVdp,
+    SrirachaVdp,
+    FlickFusion,
+    OpenSearch
+  }
+
+  public class Products
+  {
+    [JsonConverter(typeof(StringEnumConverter))]
+    public static readonly Dictionary<Category, Product[]> CategorizedProducts = new Dictionary<Category, Product[]>
+    {
+      {
+        Category.AddOns, new[]
+        {
+          Product.Bts,
+          Product.MobileLeadDriver,
+          Product.MobileLeadDriverWithGeofencing,
+        }
+      },
+      {
+        Category.PricingElements, new[]
+        {
+          Product.PersonalizedOffer,
+          Product.ChromeIncentives
+        }
+      },
+      {
+        Category.ChatProviders, new[]
+        {
+          Product.Bts
+        }
+      },
+      {
+        Category.VdpTypes, new[]
+        {
+          Product.ClassicVdp,
+          Product.SrirachaVdp
+        }
+      },
+      {
+        Category.Tpis, new[]
+        {
+          Product.FlickFusion,
+        }
+      },
+      {
+        Category.Widgets, new[]
+        {
+          Product.OpenSearch,
+        }
+      }
+    };
+  }
+}
